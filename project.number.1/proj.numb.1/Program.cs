aa6c4cc1c4330a72956151116d98133e52836c68
Nima <nima.sherafat.cj@gmail.com>
2023-05-25 19:22:13 +0430

    mabani.project.1d2

---
+++ project.number.1/proj.numb.1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proj.numb._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+
+            Console.WriteLine("please enter your name");
+            name = Console.ReadLine();
+
+            Console.WriteLine("hello" + name);
+            Console.WriteLine("press any key to exit...");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ project.number.2/proj.numb.2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proj.numb._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            Console.WriteLine("please enter a number");
+            string number = Console.ReadLine();
+            x = int.Parse(number);
+
+            x *= 3;
+
+            Console.WriteLine(x);
+            Console.ReadKey();
+
+
+        }
+    }
+}
